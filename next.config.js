e933a08ad20a5d0373fdb766877f04d8d3adc83c
Abdurabd <49178533+Abdurabd@users.noreply.github.com>
2023-07-16 17:15:14 +0100

    next.config.js update to build issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-module.exports = {
-  images: {
-    domains: ["cdn.hashnode.com"],
-  },
-  reactStrictMode: true,
-};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    /* config options here */
+    images: {
+        domains: ["cdn.hashnode.com"],
+    },
+    reactStrictMode: true,
+}
+
+module.exports = nextConfig
