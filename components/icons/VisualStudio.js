767f1ab44e889c5d0b11cbb2fa576b70b4989aa0
Abdurabd <49178533+Abdurabd@users.noreply.github.com>
2023-07-16 17:03:17 +0100

    initial commit

---
+++ components/icons/VisualStudio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VisualStudio = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128">
+        <path className="dark:text-light text-mid transition-all duration-200 ease-in-out fill-current" d="M95 2.3l30.5 12.3v98.7l-30.7 12.4-49-48.7-31 24.1-12.3-6.2V33.1l12.3-5.9 31 24.3zM14.8 45.7v37.5l18.5-19zm48.1 18.5l31.9 25.1V39z"/>
+    </svg>
+  )
+}
+
+export default VisualStudio
